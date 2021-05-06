9f5a72ed4b1bef7a9e666988793c1d6976d4568e
MarcoCestari - dev <58509253+MarcoCestari-dev@users.noreply.github.com>
2021-05-06 09:27:47 +0200

    Delete PacketLoss-Bernoulli - 4Video.bat

--- PacketLoss-Bernoulli - 4Video.bat
+++
@@ -1,4 +0,0 @@
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba netem --interface eth0 --duration 1m loss --percent 40 HostA
-
-Commento: Aumentiamo momentaneamente il rate di perdita al fine di diminuire la lunghezza del video.
-Comando Originale: "docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba netem --interface eth0 --duration 1m loss --percent 3 HostA"
